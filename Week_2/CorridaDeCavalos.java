99c83825614e460f2f8d21d7bcb09cf459d65bd4
TiagoLealPalma <tiagolealpalma@gmail.com>
2024-09-24 20:24:33 +0100

    week2_exercise2done

---
+++ Week_2/CorridaDeCavalos.java
@@ -0,0 +1,86 @@
+package Week_2;
+
+import javax.swing.*;
+import javax.xml.namespace.QName;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CorridaDeCavalos {
+    private JFrame frame;
+    private Cavalo cavalo1, cavalo2, cavalo3;
+
+    private class Cavalo extends Thread{
+        private int distance = 30; // Distancia até ao final da corrida
+        private JTextField textField;
+        private String name;
+
+        public Cavalo(JTextField textField, String name){
+            this.textField = textField;
+            this.name = name;
+        }
+
+        public void run(){
+            try {
+                // Loop principal da thread usado para decrementar o valor, com um intervalo aleatorio entre 0 e 1 segundo.
+                while (distance > 0) {
+                    distance--;
+                    textField.setText(String.valueOf(distance));
+                    sleep((long) (Math.random() * 1000) + 1);
+                }
+            }catch(InterruptedException e) {
+                System.out.println(getName() + "foi interrompida");
+            }
+            System.out.println(name + " terminou a corrida"); // Quando sai do loop, anuncia que acabou a corrida.
+        }
+    }
+
+    public CorridaDeCavalos(){
+        frame = new JFrame("Corrida de cavalos");
+        frame.setSize(200,100);
+        frame.setLocationRelativeTo(null); // Centra a janela
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel textPanel = new JPanel();
+        JTextField firstSlot = new JTextField("30");
+        firstSlot.setEditable(false);
+        JTextField secondSlot = new JTextField("30");
+        secondSlot.setEditable(false);
+        JTextField thirdSlot = new JTextField("30");
+        thirdSlot.setEditable(false);
+
+        cavalo1 = new Cavalo(firstSlot, "Cavalo 1");
+        cavalo2 = new Cavalo(secondSlot, "Cavalo 2");
+        cavalo3 = new Cavalo(thirdSlot, "Cavalo 3" );
+
+        textPanel.setLayout(new FlowLayout());
+        textPanel.add(firstSlot);
+        textPanel.add(secondSlot);
+        textPanel.add(thirdSlot);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(textPanel, BorderLayout.CENTER);
+        JButton startButton = new JButton("Start");
+        frame.add(startButton, BorderLayout.SOUTH);
+
+        startButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cavalo1.start();
+                cavalo2.start();
+                cavalo3.start();
+            }
+        });
+
+        open();
+    }
+
+    private void open(){
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CorridaDeCavalos();
+    }
+}
---
+++ Week_2/NameThread.java
@@ -0,0 +1,90 @@
+package Week_2;
+
+public class NameThread extends Thread{
+    private int identifier;
+
+    public NameThread(int identifier){
+        this.identifier = identifier;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public void run() { // Processo a correr aquando se dá o inicio da Thread
+        try {
+            for (int i = 0; i < 10; i++) {
+                // Verificar se a Thread foi interrompida ou se pode continuar a correr
+                if(Thread.interrupted()){
+                    System.out.println("Thread "+ getIdentifier() +" interrupted");
+                    return;
+                }
+
+                System.out.println("Indentifier: " + getIdentifier()); // Dar print ao nome da Thread
+                // Adormecer a Thread de 1 a 2 segundos de forma aleatória
+                    int sleepTime = ((int)(Math.random()*2) + 1); // Tempo em seconds
+                    System.out.println("Sleeping for " + sleepTime + " seconds (" + getIdentifier() + ")");
+                    sleep(sleepTime*1000L); // Transformar em milissegundos
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread "+ getIdentifier() +" interrupted");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        // Primeiro Teste
+        System.out.println("Primeiro teste:");
+        NameThread thread = new NameThread(1);
+        NameThread thread2 = new NameThread(2);
+        thread.start();
+        thread2.start();
+
+        // Esperar que ambas as Threads terminem
+        try { // Encapsular com try/catch para que se possa apanhar algum erro, sem que o programa encerre
+            thread.join(); // Thread 1 termine
+            thread2.join(); // Thread 2 termine
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Teste 1 acabou");
+
+        // Segundo Teste
+        System.out.println("Segundo teste:");
+        NameThread secondThread = new NameThread(1);
+        NameThread secondThread2 = new NameThread(2);
+        secondThread.start();
+        secondThread2.start();
+
+        // Esperar que ambas as Threads terminem ou que passem 4 segundos
+        long maxWaitTime = 4000;
+        try {
+            long startTime = System.currentTimeMillis();                // Apanhar o tempo de inicio da espera
+            secondThread.join(maxWaitTime);                             // Thread 1 termine ou passe 4 segundos
+            secondThread.interrupt();
+
+            long timeLeft = maxWaitTime -                               // Subtrair ao tempo maximo de
+                    (System.currentTimeMillis() - startTime);           // espera o tempo que a primeira thread demorou
+                                                                        // (no caso de ter sido menos de 4 segundos)
+
+            System.out.println("Tempo restante de espera: " + timeLeft);
+            if(timeLeft > 0) { // Se ainda houver tempo restante
+                secondThread2.join(timeLeft); // Se houver tempo restante, espera que a thread termine ou que esse tempo passe
+                secondThread2.interrupt();
+            }else secondThread2.interrupt();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Teste 2 acabou");
+    }
+}
